ed419e9e659e76bbfb49a1d52d85d21a65d18df0
roldel <74282569+RolandDel@users.noreply.github.com>
2021-04-16 05:27:48 +0000

    ADD STATIC_ROOT in settings.py

--- config/settings.py
+++ config/settings.py
@@ -126,6 +126,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = str(BASE_DIR.joinpath('static'))
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
